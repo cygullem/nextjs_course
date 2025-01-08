f232a22ce839a7776b8c40c926621257cfef5c9d
cygullem <crmchs.gullem.clydecyril@gmail.com>
2025-01-08 15:16:37 +0800

    Server Actions and revalidation

--- src/app/mock-users/page.tsx
+++ src/app/mock-users/page.tsx
@@ -1,60 +1,52 @@
-"use client";
-import { useState, useEffect } from "react";
+import { revalidatePath  } from "next/cache";
 
 type MockUser = {
     id: number;
     name: string;
 };
 
-export default function UsersClient() {
-    const [users, setUsers] = useState<MockUser[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
+export default async function MockUsers() {
+    const res = await fetch("https://677e1a9094bde1c1252a6027.mockapi.io/users");
+    const users = await res.json();
 
-    useEffect(() => {
-        async function fetchUsers() {
-            try {
-                const response = await fetch(
-                    "https://677e1a9094bde1c1252a6027.mockapi.io/users"
-                );
-                if (!response.ok) throw new Error("Failed to fetch users");
-                const data = await response.json();
-                setUsers(data);
-            } catch (err) {
-                setError("Failed to fetch users");
-                if (err instanceof Error) {
-                    setError(`Failed to fetch users: ${err.message}`);
-                }
-            } finally {
-                setLoading(false);
+    async function addUser(formData: FormData) {
+        "use server"
+
+        const name = formData.get("name")
+        const res = await fetch(
+            "https://677e1a9094bde1c1252a6027.mockapi.io/users",
+            {  
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ name }),
             }
-        }
-
-        fetchUsers();
-    }, []);
-
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>{error}</div>;
+        );
+        const newUser = await res.json();
+        revalidatePath("/mock-users");
+        console.log(newUser);
+    }
 
     return (
-        <div className="py-10">
-            <form className="mb-4">
-                <input 
-                    className="border p-2 mr-2"
-                    type="text" 
-                    name="name" 
-                    required 
+        <div className="p-4 bg-slate-700">
+            <form action={addUser} className="mb-8">
+                <input
+                    className="border p-2 mr-2 rounded-lg"
+                    type="text"
+                    name="name"
+                    required
                 />
-                <button 
-                    className="bg-blue-500 text-white py-2 rounded-md"
+                <button
+                    className="bg-blue-500 text-white p-2 rounded-lg"
                     type="submit"
                 >
                     Add User
                 </button>
             </form>
 
-            <div className="p-4 grid grid-cols-3 gap-4 h-[100vh]">
-                {users.map((user) => (
+            <div className="grid grid-cols-3 gap-4 h-[100vh]">
+                {users.map((user: MockUser) => (
                     <li
                         key={user.id}
                         className="flex items-center justify-center p-4 bg-white shadow-md rounded-lg text-gray-700 cursor-pointer"
