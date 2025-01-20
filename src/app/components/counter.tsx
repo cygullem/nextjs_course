bacdecfcd2d5297e0ccef2529a21ff9b19837f40
cygullem <crmchs.gullem.clydecyril@gmail.com>
2025-01-20 10:59:00 +0800

    auth status

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -1,9 +1,20 @@
 "use client";
 import { useState } from "react";
+// import { useAuth, useUser } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 
 export const Counter = () => {
+
+    const { isLoaded, userId} = useAuth();
+    // const { isLoaded, userId, sessionId, getToken} = useAuth();
+    // const { isLoaded, isSignedIn, user} = useUser();
+
     console.log("Counter component");
     const [count, setCount] = useState(0);
+
+    if(!isLoaded || !userId) {
+        return null;
+    }
 
     return (
         <button onClick={() => setCount(count + 1)}>
